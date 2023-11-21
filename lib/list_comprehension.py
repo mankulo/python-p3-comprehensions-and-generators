65f6543532389647ab38b916f4decf9afe14756e
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-11-22 02:07:50 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,11 @@
 
 def return_evens(num_list):
     pass
+    even_num = [n  for n in num_list if n % 2 == 0 or  [] ]
+    return even_num
 
 def make_exclamation(sentence_list):
     pass
+    sentence_string = [e +"!" for e in sentence_list or [] ]
+    
+    return sentence_string
